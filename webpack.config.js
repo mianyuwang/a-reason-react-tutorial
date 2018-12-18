9f1aa38f8006aaa33e5fe6f77307083b5440f904
Mianyu Wang <mwang57@bloomberg.net>
2018-12-17 22:43:33 -0500

    Improve Step 0

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 module.exports = {
+  mode: 'development',
   entry: './lib/js/src/Main.js',
+  devServer: {
+    contentBase: './public',
+    compress: true,
+    port: 9000
+  },
   output: {
     path: __dirname + '/public',
-    filename: 'bundle.js',
-  },
+    filename: 'bundle.js'
+  }
 };
-
